00ec12e8e2a1c5e2846990245654c73c95bf6102
DiegoM <medeldiego47@yahoo.com>
2023-01-12 21:13:53 -0600

    finished current weather grab from api but key is 401 not authorized

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 const API="38690b59e3cfde574e8eb53ee48ef3c8";
 var button = document.getElementById('submit');
 
-document.getElementById("submit").onclick= grabCity();
+document.getElementById("submit").onclick=grabCity
 function grabCity(){
-    var city= document.getElementById("cityName");
- if (city == "") {
-    alert("Please enter a city name")
-  }else{
+    event.preventDefault();
+    let city = $("#cityName").val().trim();
+ if(city){
    ApiCall(city);
-  }
+  } else (alert('enter a city name'))
+  console.log('works')
 };
 
 function ApiCall(city) {
@@ -18,7 +18,8 @@ function ApiCall(city) {
     .then((data)=>{
         return data.json();
     }).then((data)=>{
-        var weatherUrl =`https://api.openweathermap.org/data/3.0/onecall?lat=${data[0].lat}&lon=${data[0].lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${API}`;
+        var weatherUrl =`https://api.openweathermap.org/data/3.0/onecall?lat=${data[0].lat}&lon=${data[0].lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${API}
+        `;
 
         fetch(weatherUrl).then((data)=>{return data.json();}).then((data)=>{
             save(city);
@@ -26,7 +27,11 @@ function ApiCall(city) {
 
             document.getElementById('current-city').textContent=city;
              document.getElementById('date').textContent=new Date(data.current.dt*1000);
-              document.getElementById('current-city').textContent=city;
+              document.getElementById('currentIcon').setAttribute('src', `https://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`);
+              document.getElementById('tempNow').textContent=data.current.temp;
+              document.getElementById('windNow').textContent=data.current.wind_speed;
+              document.getElementById('humidityNow').textContent=data.current.humidity;
+              document.getElementById('uvNow').textContent=data.current.uvi;
 
         })
     })
